c2e0a358571f12c05aac787ef9be1e2c03382e42
Sejal Thakur <sejalthakurcs@gmail.com>
2026-03-18 07:51:51 +0530

    Dockerfile

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / '../database/db.sqlite3',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
