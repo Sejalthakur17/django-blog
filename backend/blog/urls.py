b8474daed666643bbb67b7d7879540f50ef60c20
Sejal Thakur <sejalthakurcs@gmail.com>
2026-03-18 22:27:35 +0530

    Added ECR

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,20 +1,7 @@
 from django.urls import path
-from backend.blog import views   
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
-from django.urls import path
 from .views import PostListCreateAPIView, PostDetailAPIView
-from django.urls import path
-from .views import get_posts
-
 
 urlpatterns = [
-    path('posts/', get_posts),
-    path('api/posts/', PostListCreateAPIView.as_view()),
-    path('api/posts/<int:pk>/', PostDetailAPIView.as_view()),   path('', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='blog-about')
+    path('posts/', PostListCreateAPIView.as_view()),
+    path('posts/<int:pk>/', PostDetailAPIView.as_view()),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,87 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
-from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Post
 
-@api_view(['GET'])
-def get_posts(request):
-    posts = Post.objects.all().values()
-    return Response(list(posts))
 
-def home(request):
-    context = {'posts' : Post.objects.all() }
-    return render(request, 'blog/home.html', context)
+# GET all posts + CREATE new post
 class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
+# GET single post + UPDATE + DELETE
 class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-class PostListView(ListView):
-    model = Post
-    template_name= 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted'] 
-    paginate_by = 3
-
-class UserPostListView(ListView):
-    model = Post
-    template_name= 'blog/user_posts.html'
-    context_object_name = 'posts'
-    paginate_by = 3
-
-    def get_queryset(self):
-        user = get_object_or_404(User, username = self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
-
-class PostDetailView(DetailView):
-    model = Post
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        else:
-            return False
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        else:
-            return False
-
-def about(request):
-    return render(request, 'blog/about.html')
-
-
