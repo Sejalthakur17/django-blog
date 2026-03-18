668adb6fbbf1927f8755425db6bff69b2c02706a
Sejal Thakur <sejalthakurcs@gmail.com>
2026-03-18 22:32:07 +0530

    Added ECR

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,14 +2,10 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-
-# GET all posts + CREATE new post
 class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-
-# GET single post + UPDATE + DELETE
 class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
