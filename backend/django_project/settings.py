cd04c7e25f5f6244cd728ef9c8295547c6782ccb
Sejal Thakur <sejalthakurcs@gmail.com>
2026-03-18 19:13:33 +0530

    Added ECR

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / '../frontend/static',
+    BASE_DIR / 'frontend/static', 
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
